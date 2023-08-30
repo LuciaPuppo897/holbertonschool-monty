ff64894f24cde9a3ce1284743b9a6786a33065d0
German1127 <kleverk2023@gmail.com>
2023-08-30 06:01:16 -0700

    update monty.h

--- monty.h
+++ monty.h
@@ -1,15 +1,6 @@
-
 #ifndef MONTY_H
 #define MONTY_H
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <string.h>
-
+/* structures */
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
  * @n: integer
@@ -21,11 +12,10 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -36,13 +26,34 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+/* standard libraries */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#include <ctype.h>
 
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+extern unsigned int isFail;
+/* function prototypes */
+int read_file(FILE *fd);
+int parse_line(char *line, stack_t **h, unsigned int lnum);
+
+void push(stack_t **h, unsigned int ln);
+void pall(stack_t **h, unsigned int ln);
+void pint (stack_t **h, unsigned int ln);
+void pop(stack_t **h, unsigned int ln);
+void swap(stack_t **h, unsigned int ln);
+void add(stack_t **h, unsigned int ln);
+void nop(stack_t **h, unsigned int ln);
+
+void (*get_op(char *str))(stack_t **stack, unsigned int line_number);
+void free_stack(stack_t *head);
 #endif
