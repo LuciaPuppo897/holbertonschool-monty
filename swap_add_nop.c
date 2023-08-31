6dcac9af503d3c452c95f37faa3808e69e4d4be6
LuciaPuppo897 <alerome897@gmail.com>
2023-08-31 06:32:20 -0700

    stderr

--- swap_add_nop.c
+++ swap_add_nop.c
@@ -9,7 +9,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 {
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
-		printf("L%d: can't swap, stack too short\n", line_number);
+		fprintf(stderr,"L%d: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
@@ -36,7 +36,7 @@ void _add(stack_t **stack, unsigned int line_number)
 
 	if (*stack == NULL || (*stack)->next == NULL)
 {
-	printf("L%d: can't add, stack too short\n", line_number);
+	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
